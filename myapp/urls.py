9fe96ddd73c53c404b0f1cb321526c5870afb76c
haolin <kaoxi998533@gmail.com>
2024-07-01 00:58:22 +0800

    changed mode of separation to directly obtaining files from backend

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('decompose/to_vocals', decompose_to_vocals),
-    path('decompose/to_drums', decompose_to_drums),
-    path('decompose/to_bass', decompose_to_bass),
-    path('decompose/to_others', decompose_to_others),
+    path('decompose/to_vocals/<song_id>', decompose_to_vocals),
+    path('decompose/to_drums/<song_id>', decompose_to_drums),
+    path('decompose/to_bass/<song_id>', decompose_to_bass),
+    path('decompose/to_others/<song_id>', decompose_to_others),
     path('get_song/<index>/<song_id>', get_song),
 ]
 
--- myapp/views.py
+++ myapp/views.py
@@ -11,24 +11,27 @@ from django.http import JsonResponse
 # Configure logging
 logger = logging.getLogger(__name__)
 
-def decompose(request, dest_track_type):
+def decompose(request, dest_track_type, song_id):
     try:
-        if 'file' not in request.FILES:
-            return Response({'error': 'No file uploaded'}, status=400)
+        # if 'file' not in request.FILES:
+        #    return Response({'error': 'No file uploaded'}, status=400)
 
-        file = request.FILES['file']
-        with tempfile.NamedTemporaryFile(delete=False, suffix='.mp3') as temp_input:
-            for chunk in file.chunks():
-                temp_input.write(chunk)
-            temp_input_path = temp_input.name
+        #file = request.FILES['file']
+        #with tempfile.NamedTemporaryFile(delete=False, suffix='.mp3') as temp_input:
+        #    for chunk in file.chunks():
+        #        temp_input.write(chunk)
+        #    temp_input_path = temp_input.name
 
+        formatted_id = str(song_id).zfill(6)
+        directory = formatted_id[:3]
+        file_path = "music/songs/{}/{:06}.mp3".format(directory, song_id)
         output_dir = tempfile.mkdtemp()
-        demucs_command = f'--mp3 --two-stems {dest_track_type} -o {output_dir} -n htdemucs {temp_input_path}'
+        demucs_command = f'--mp3 --two-stems {dest_track_type} -o {output_dir} -n htdemucs {file_path}'
         demucs.separate.main(shlex.split(demucs_command))
 
         logger.info(f"Contents of the output directory: {os.listdir(output_dir)}")
 
-        track_path = os.path.join(output_dir, 'htdemucs', os.path.basename(temp_input_path).replace('.mp3', ''), f'{dest_track_type}.mp3')
+        track_path = os.path.join(output_dir, 'htdemucs', os.path.basename(file_path).replace('.mp3', ''), f'{dest_track_type}.mp3')
 
         if os.path.exists(track_path):
             response = FileResponse(open(track_path, 'rb'), content_type='audio/mpeg')
@@ -36,7 +39,6 @@ def decompose(request, dest_track_type):
 
             # Clean up the temporary files
             os.remove(track_path)
-            os.remove(temp_input_path)
 
             return response
         else:
@@ -48,20 +50,20 @@ def decompose(request, dest_track_type):
         return Response({'error': str(e)}, status=500)
 
 @api_view(['POST'])
-def decompose_to_vocals(request): 
-    return decompose(request, 'vocals')
+def decompose_to_vocals(request, song_id): 
+    return decompose(request, 'vocals', song_id)
 
 @api_view(['POST'])
-def decompose_to_bass(request):
-    return decompose(request, 'bass')
+def decompose_to_bass(request, song_id):
+    return decompose(request, 'bass', song_id)
 
 @api_view(['POST'])
-def decompose_to_drums(request):
-    return decompose(request, 'drums')
+def decompose_to_drums(request, song_id):
+    return decompose(request, 'drums', song_id)
 
 @api_view(['POST'])
-def decompose_to_others(request):
-    return decompose(request, 'others')
+def decompose_to_others(request, song_id):
+    return decompose(request, 'others', song_id)
 
 @api_view(['POST'])
 def get_song(request, index, song_id):
